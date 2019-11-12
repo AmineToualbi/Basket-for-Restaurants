11b705b07d9f0036c4429188ed65681f75a87f32
Amine <amine.toualbi@yahoo.fr>
2019-11-12 10:43:54 -0700

    Firebase setup

---
+++ app/src/main/java/com/myapps/toualbiamine/basketbusiness/MainActivity.java
@@ -0,0 +1,13 @@
+package com.myapps.toualbiamine.basketbusiness;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
